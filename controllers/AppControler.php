14acaa8e3543155794af77bba080c923fa48b534
Rashitov Rishat <wildrage88@gmail.com>
2019-07-29 14:45:57 +0300

    appcontroler

---
+++ controllers/AppControler.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class AppControler extends Controller
+{
+    public function debug($arr){
+        echo "<pre>".print_r($arr, true)."</pre>";
+    }
+}
+
+function debug($arr){
+    echo "<pre>".print_r($arr, true)."</pre>";
+}
